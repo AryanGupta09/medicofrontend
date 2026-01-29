09137ce511340f2241d866016062a0f7e979570a
Aryan Gupta <5guptaaryan10@gmail.com>
2026-01-29 20:45:46 +0530

    Fix: Correct Tokens import with capital T

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Doctors from "./Doctors";
 import Patients from "./Patients";
 import BookToken from "./BookToken";
-import Tokens from "./Tokens";
+import Tokens from "./tokens";
 
 export default function App() {
   return (
