8efe37868829368296f6fa0aa17456311c1a1e22
Aryan Gupta <5guptaaryan10@gmail.com>
2026-01-29 20:57:27 +0530

    Fix: Update API URL to production backend

--- src/api.js
+++ src/api.js
@@ -1,5 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
-export default axios.create({
-  baseURL: "http://localhost:5000"
-});
+const API_URL = 'https://medicobackend-drhi.onrender.com';
+
+export const getDoctors = () => axios.get(`${API_URL}/api/doctors`);
+export const getPatients = () => axios.get(`${API_URL}/api/patients`);
+export const getTokens = () => axios.get(`${API_URL}/api/tokens`);
+export const getSlots = () => axios.get(`${API_URL}/api/slots`);
+export const bookToken = (data) => axios.post(`${API_URL}/api/tokens`, data);
